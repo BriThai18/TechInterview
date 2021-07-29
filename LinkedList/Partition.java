13596f8d462bcf81958cc8660292196de3a84d2c
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-28 21:38:28 -0400

    Update Partition.java

--- LinkedList/Partition.java
+++ LinkedList/Partition.java
@@ -1,1 +1,47 @@
-
+public class Partition2{
+  static Node head; 
+  static class Node{
+    int data;
+    Node next;
+    
+    Node(int num){
+      data = num;
+      next = null; 
+    }
+  }
+  static Node partition(Node head, int x){
+    Node beforeStart = null;
+    Node beforeEnd = null;
+    Node afterStart = null;
+    Node afterEnd = null;
+    
+    while(head != null){
+      Node next = head.next;
+      head.next = null; 
+      if(head.data < x){
+        if(beforeStart == null){
+          beforeStart = head;
+          beforeEnd = beforeStart; 
+        }else{
+          beforeEnd.next = head;
+          beforeEnd = head; 
+        }
+      }else{
+        if(afterStart == null){
+          afterStart = head;
+          afterEnd = afterStart; 
+        }else{
+          afterEnd.next = head;
+          afterEnd = head; 
+        }
+      }
+    }
+    
+    if(beforeStart == null){
+      return afterStart;
+    }
+    
+    beforeEnd.next = afterStart;
+    return beforeStart; 
+  }
+}
