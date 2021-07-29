71acef4abfc964ff20c76c3936bd4c04b13410e8
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-28 21:04:46 -0400

    Update DeleteMiddleNode.java

--- LinkedList/DeleteMiddleNode.java
+++ LinkedList/DeleteMiddleNode.java
@@ -1,1 +1,34 @@
+public class DeleteMiddleNode{
+  static Node head;
+  static class Node{
+    int data;
+    Node next;
+    
+    Node(int num){
+      data = num;
+      next = null; 
+    }
+  }
+  static Node deleteMiddle(Node head){
+    if(head == null){
+      return null; 
+    }
+    if(head.next == null){
+      return null; 
+    }
+    
+    Node slow = head;
+    Node fast = head;
+    Node prev = null; 
+    
+    while(fast != null && fast.next != null){
+      fast = fast.next.next;
+      prev = slow; 
+      slow = slow.next;
+    }
+    prev.next = slow.next;
+    return head; 
+  }
+}
 
+//Runtime o(n)
