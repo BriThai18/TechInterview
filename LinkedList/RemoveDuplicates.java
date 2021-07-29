f646751ea8435a2f166742bd826614bb761c585f
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-28 21:46:09 -0400

    Update RemoveDuplicates.java

--- LinkedList/RemoveDuplicates.java
+++ LinkedList/RemoveDuplicates.java
@@ -1,1 +1,29 @@
+class RemoveDuplicates{
+  static Node head;
+  static class Node{
+    int data;
+    Node next;
+    
+    Node(int num){
+      data = num;
+      next = null; 
+    }
+  }
+  void removeDuplicates(){
+    Node current = head;
+    
+    while(current != null){
+      Node runner = current;
+      while(runner.next != null){
+        if(runner.next.data == current.data){
+          runner.next = runner.next.next; 
+        }else{
+          runner = runner.next; 
+        }
+      }
+      current = current.next; 
+    }
+  }
+}
 
+//Runtime o(n^2)
