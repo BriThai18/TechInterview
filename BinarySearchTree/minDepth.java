09fc6777b808f58cb91e962b57d270c211457f10
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-30 13:44:57 -0400

    Update minDepth.java

--- BinarySearchTree/minDepth.java
+++ BinarySearchTree/minDepth.java
@@ -1,1 +1,20 @@
-
+class Node{
+  int data;
+  Node left, right;
+  
+  Node(int data){
+    this.data = data;
+    left = right = null; 
+  }
+}
+public class minDepth{
+  Node root;
+  int getMinDepth(Node root){
+    if(root == null){
+      return 0; 
+    }
+    int left = getMinDepth(node.left);
+    int right = getMinDepth(node.right);
+    return Math.min(left, right) + 1; 
+  }
+}
