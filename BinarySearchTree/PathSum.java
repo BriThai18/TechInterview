f539c232bfab2885d4003aa61515c4bddf069f56
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-30 12:59:04 -0400

    Update PathSum.java

--- BinarySearchTree/PathSum.java
+++ BinarySearchTree/PathSum.java
@@ -1,1 +1,21 @@
-
+class Node{
+  int data;
+  Node left, right;
+  
+  Node(int data){
+    this.data = data;
+    left = right = null; 
+  }
+}
+public class PathSum{
+  Node root;
+  boolean getPath(Node node, int target){
+    if(node == null){
+      return false; 
+    }
+    if(node.data == target && node.left == null && node.right == null){
+      return true; 
+    }
+    return getPath(node.left, target-node.data) || getPath(node.right, target-node.data);
+  }
+}
