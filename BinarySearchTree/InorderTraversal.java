e0afb87a901997dc4cb82c7eef35e11bb7064659
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-29 21:24:46 -0400

    Update InorderTraversal.java

--- BinarySearchTree/InorderTraversal.java
+++ BinarySearchTree/InorderTraversal.java
@@ -1,1 +1,26 @@
+public class InorderTraversal{
+  static class Node{
+    int data; 
+    Node left, right;
+   
+    public Node(int data){
+      this.data = data; 
+    }
+  }
+  static Stack<Node> stack;
+  static void inOrderTraversal(Node root){
+    stack = new Stack<Node>();
+    
+    while(!stack.isEmpty() || root != null){
+      while(root != null){
+        stack.push(root);
+        root = root.left; 
+      }
+      root = stack.pop();
+      System.out.print(root.data + " ");
+      root = root.right; 
+    }
+  }
+}
 
+//Runtime o(n)
