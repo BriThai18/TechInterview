df4a3fa065b1cc982b380f9d363c654cfca2b8b4
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-30 19:53:13 -0400

    Update LowestCommonAncestor.java

--- BinarySearchTree/LowestCommonAncestor.java
+++ BinarySearchTree/LowestCommonAncestor.java
@@ -1,1 +1,24 @@
-
+class Node{
+  int data;
+  Node left, right;
+  
+  Node(int data){
+    this.data = data;
+    left = right = null; 
+  }
+}
+public class LowestCommonAncestor{
+  Node root;
+  Node lowestCommonAncestor(Node node, int node1, int node2){
+    if(node == null){
+      return null; 
+    }
+    if(node.data > node1 && node.data > node2){
+      return lowestCommonAncestor(node.left, node1, node2);
+    }
+    if(node.data < node1.data && node.data < node2){
+      return lowestCommonAncestor(node.right, node1, node2);
+    }
+    return node; 
+  }
+}
