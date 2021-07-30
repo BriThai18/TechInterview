f16b7c4ecb3dde1fb25c514224c3c315a89a77b3
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-30 13:29:33 -0400

    Update ValidateBST.java

--- BinarySearchTree/ValidateBST.java
+++ BinarySearchTree/ValidateBST.java
@@ -1,1 +1,30 @@
+class Node{
+  int data;
+  Node left, right;
+  
+  Node(int data){
+    this.data = data;
+    left = right = null; 
+  }
+}
+public class ValidateBST{
+  Node root; 
+  boolean isValidBST(Node node){
+    if(node == null){
+      return true;
+    }
+    return checkValid(node, null, null); 
+  }
+  boolean checkValid(Node node, Integer min, Integer max){
+    if(node == null){
+      return true; 
+    }
+    if(min != null && node.data <= min || max != null && node.data >= max){
+      return false; 
+    }
+    return checkValid(node.left, min, node.data) && checkValid(node.right, node.data, max); 
+  }
+}
 
+//Runtime o(n)
+//Space o(1)
