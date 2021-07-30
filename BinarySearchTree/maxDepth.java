e62b85d55ed08b05147eee70b21632284a11b45f
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-30 13:36:34 -0400

    Update maxDepth.java

--- BinarySearchTree/maxDepth.java
+++ BinarySearchTree/maxDepth.java
@@ -1,1 +1,22 @@
+class Node{
+  int data;
+  Node left, right;
+  
+  Node(int data){
+    this.data = data;
+    left = right = null; 
+  }
+}
+public class maxDepth{
+  Node root; 
+  int getMaxDepth(Node root){
+    if(root == null){
+      return 0; 
+    }
+    int left = getMaxDepth(root.left);
+    int right = getMaxDepth(root.right);
+    return Math.max(left, right) + 1; 
+  }
+}
 
+//Runtime o(n)
