125940af80e036206412ede0e86a7ff407a8f5f1
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-29 21:33:07 -0400

    Update PreOrderTraversal.java

--- BinarySearchTree/PreOrderTraversal.java
+++ BinarySearchTree/PreOrderTraversal.java
@@ -1,1 +1,26 @@
+public class PreOrderTraversal{
+  static class Node{
+    int data; 
+    Node left, right;
+    
+    public Node(int data){
+      this.data = data; 
+    }
+  }
+  static Stack<Node> stack; 
+  static void preOrderTraversal(Node root){
+    stack = new Stack<Node>();
+    
+    while(!stack.isEmpty() || root != null){
+      while(root != null){
+        System.out.print(root.data);
+        stack.push(root);
+        root = root.left; 
+      }
+      root = stack.pop();
+      root = root.right; 
+    }
+  }
+}
 
+//Runtime o(n)
