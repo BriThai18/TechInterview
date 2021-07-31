da97c6a26f4be48f627eb718436aab56614ef061
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-07-30 20:32:01 -0400

    Update BalancedBinarySearchTree.java

--- BinarySearchTree/BalancedBinarySearchTree.java
+++ BinarySearchTree/BalancedBinarySearchTree.java
@@ -1,1 +1,30 @@
-
+class Node{
+  int data;
+  Node left, right;
+  
+  Node(int data){
+    this.data = data;
+    left = right = null; 
+  }
+}
+public class BalancedBinarySearchTree{
+  Node root;
+  boolean isBalanced(Node node){
+    if(node == null){
+      return true; 
+    }
+    return getHeight(node) != -1; 
+  }
+  int getHeight(Node node){
+    if(node == null){
+      return 0; 
+    }
+    int left = getHeight(node.left); 
+    int right = getHeight(node.right);
+    
+    if(left == -1 || right == -1 || Math.abs(left - right) > 1){
+      return -1; 
+    }
+    return Math.max(left, right) + 1; 
+  }
+}
