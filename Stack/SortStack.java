9043b121045c3c3fed38186a0d28335ec7294259
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-03 23:07:48 -0400

    Update SortStack.java

--- Stack/SortStack.java
+++ Stack/SortStack.java
@@ -1,1 +1,20 @@
+public class SortStack{
+  public static Stack<Integer> SortStack(Stack<Integer> stack){
+    if(stack == null || stack.isEmpty()){
+      return; 
+    }
+    
+    Stack<Integer> tempStack = new Stack<>(); 
+    
+    while(!stack.isEmpty()){
+      int temp = stack.pop();
+      while(!tempStack.isEmpty() && temp > tempStack.peek()){
+        stack.push(tempStack.pop());
+      }
+      tempStack.push(temp); 
+    }
+    return tempStack; 
+  }
+}
 
+//Runtime o(n^2)
