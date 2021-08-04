5f18424729ecd8221d1a1ee22b12fd2a020d5e99
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-03 22:58:51 -0400

    Update MinStack.java

--- Stack/MinStack.java
+++ Stack/MinStack.java
@@ -1,1 +1,21 @@
-
+public class MinStack{
+  Stack<Integer> stack = new Stack<>();
+  Stack<Integer> min = new Stack<>();
+  
+  public void push(int x){
+    if(min.isEmpty() || x <= min.peek()){
+      min.push(x);
+    }
+    stack.push(x);
+  }
+  public void pop(){
+    stack.pop();
+    min.pop(); 
+  }
+  public int top(){
+    return stack.peek();
+  }
+  public int min(){
+    return min.peek(); 
+  }
+}
