0b33011427e7442ae9601725086afc5fd6b525e5
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-04 19:56:51 -0400

    Update ValidParentheses.java

--- Stack/ValidParentheses.java
+++ Stack/ValidParentheses.java
@@ -1,1 +1,40 @@
-
+public class ValidParentheses{
+  public static boolean isValid(String s){
+    Stack<Character> stack = new Stack<>();
+    char x; 
+    
+    for(int i = 0; i < s.length(); i++){
+      if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+        stack.push(s.charAt(i));
+        continue; 
+      }
+      if(stack.isEmpty()){
+        return false; 
+      }
+      
+      switch(s.charAt(i)){
+        case ')':
+          x = stack.peek();
+          stack.pop(); 
+          if(x == '[' || x == '{'){
+            return false; 
+          }
+          break;
+        case ']':
+          x = stack.peek();
+          stack.pop();
+          if(x == '{' || x == '('){
+            return false; 
+          }
+          break; 
+        case '}':
+          x = stack.peek();
+          stack.pop(); 
+          if(x == '[' || x ==  '('){
+            return false; 
+          }
+      }
+    }
+    return stack.isEmpty(); 
+  }
+}
