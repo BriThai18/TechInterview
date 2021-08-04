fed0d9cefc26ced661b62ec2156d314e580c7f9b
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-03 22:38:21 -0400

    Update SelectionSort.java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,1 +1,24 @@
+public class SelectionSort{
+  public static void sort(int[] array){
+    int minVal, minIndex = 0; 
+    int n = array.length;
+    
+    for(int i = 0; i < n; i++){
+      minIndex = i;
+      minVal = array[i];
+      for(int j = 1; j < n; j++){
+        if(array[j] < minVal){
+          minVal = array[j];
+          minIndex = j; 
+        }
+      }
+      if(minVal < array[i]){
+        int temp = array[i]; 
+        array[i] = array[minIndex];
+        array[minIndex] = temp; 
+      }
+    }
+  }
+}
 
+//Runtime o(n^2)
