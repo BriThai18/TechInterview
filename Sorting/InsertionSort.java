cabddd84e96e82e624c53a87895e8ce715d55ac5
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-04 20:08:23 -0400

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,1 +1,18 @@
+public class InsertionSort{
+  public static void sort(int[] array){
+    int n = array.length;
+    
+    for(int i = 1; i < n; i++){
+      int key = array[i]; 
+      int j = i - 1; 
+      while(j >= 0 && array[j] > key){
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp; 
+        j--; 
+      }
+    }
+  }
+}
 
+//Runtime o(n^2)
