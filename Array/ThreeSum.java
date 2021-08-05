9c60ba0271584e80a7c2534c2f7c839223119d03
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-05 15:07:45 -0400

    Update ThreeSum.java

--- Array/ThreeSum.java
+++ Array/ThreeSum.java
@@ -1,1 +1,29 @@
+public class ThreeSum{
+  public static List<List<Integer>> threeSum(int[] nums){
+    Set<List<Integer>> set = new HashSet<>();
+    
+    if(nums.length < 3){
+      return Collections.emptyList();
+    }
+    
+    Arrays.sort();
+    for(int i = 0; i < nums.length - 2; i++){
+      int j = i + 1;
+      int k = nums.length - 1; 
+      while(j < k){
+        int sum = nums[i] + nums[j] + nums[k];
+        if(sum == 0){
+          set.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+        }else if(sum > 0){
+          k--; 
+        }else if(sum < 0){
+          j++; 
+        }
+      }
+    }
+    return new ArrayList<>(set); 
+  }
+}
 
+//Runtime o(n^2)
+//Space o(n)
