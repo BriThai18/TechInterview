1b2cc62a5343f9a06f9999e53b783fe48da866c4
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-04 20:03:59 -0400

    Update RemoveAdjacentDuplicate.java

--- Stack/RemoveAdjacentDuplicate.java
+++ Stack/RemoveAdjacentDuplicate.java
@@ -1,1 +1,19 @@
-
+public class RemoveAdjacentDuplicate{
+  public static String removeDuplicates(String s){
+    Stack<Character> stack = new Stack<>();
+    
+    for(char x: s.toCharArray()){
+      if(!stack.isEmpty() && x == stack.peek()){
+        stack.pop();
+      }else{
+        stasck.push(); 
+      }
+    }
+    
+    StringBuilder sb = new StringBuilder;
+    for(char x: stack){
+      sb.append(x); 
+    }
+    return sb.toString(); 
+  }
+}
