04e5a16d9bef8757873a848f9ed6740bad5c5ce5
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-05 23:07:30 -0400

    Update BFS.java

--- BreadthFirstSearch/BFS.java
+++ BreadthFirstSearch/BFS.java
@@ -1,1 +1,39 @@
+public class BFS{
+  private int vertices;
+  private LinkedList<Integer> adj[];
+  
+  BFS(int v){
+    vertices = v; 
+    adj = new LinkedList[v];
+    for(int i = 0; i < v; i++){
+      adj[i] = new LinkedList<>();
+    }
+  }
+  void addEdge(int v, int w){
+    adj[v].add(w); 
+  }
+  void breadthFirstSearch(int s){
+    boolean visited[] = new boolean[vertices];
+    LinkedList<Integer> queue = new LinkedList<>();
+    
+    visited[s] = true;
+    queue.add(s);
+    
+    while(queue.size != 0){
+      s = queue.poll(); 
+      System.out.print(s + " ");
+      
+      Iterator<Integer> it = adj[s].listIterator();
+      while(it.hasNext()){
+        int n = it.next(); 
+        if(!visited[n]){
+          visited[n] = true; 
+          queue.add(n); 
+        }
+      }
+    }
+  }
+}
 
+//Runtime o(v + e) 
+//Space o(v)
