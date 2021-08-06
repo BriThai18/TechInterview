d18d6a1eda3706c3a8e2e576c765734980486f10
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-05 22:30:12 -0400

    Update SortArrayByParity.java

--- Array/SortArrayByParity.java
+++ Array/SortArrayByParity.java
@@ -1,1 +1,18 @@
-
+public class SortArrayByParity{
+  public int[] sortArrayByParity(int[] nums){
+    int[] res = new int[nums.length];
+    int index = 0;
+    
+    for(int i = 0; i < nums.length; i++){
+      if(nums[i] % 2 == 0){
+        res[index++] = nums[i]; 
+      }
+    }
+    for(int j = 0; j < nums.length; j++){
+      if(nums[j] % 2 == 1){
+        res[index++] = nums[j];
+      }
+    }
+    return res; 
+  }
+}
