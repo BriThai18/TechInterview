01f07d65a79132036de5d9785e33dc754c5f315f
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-05 22:00:34 -0400

    Update Subsets.java

--- Array/Subsets.java
+++ Array/Subsets.java
@@ -1,1 +1,17 @@
+public class Subsets{
+  public static List<List<Integer>> subsets(int[] nums){
+    List<List<Integer>> res = new ArrayList<>();
+    res.add(new ArrayList<>());
 
+    for(int num: nums){
+      List<List<Integer>> temp = new ArrayList<>();
+      for(List<Integer> in : res){
+        List<Integer> a = new ArrayList<>(in);
+        a.add(num);
+        temp.add(a);
+      }
+      res.addAll(temp);
+    }
+    return res; 
+  }
+}
