53a83a291831363bda87290f145c0657218d2fad
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-07 22:20:53 -0400

    Update DepthFirstSearch.java

--- DepthFirstSearch/DepthFirstSearch.java
+++ DepthFirstSearch/DepthFirstSearch.java
@@ -1,1 +1,51 @@
+public class DepthFirstSearch{
+  private LinkedList<Integer> adjList[];
+  private boolean visited[];
+  private Stack<Integer> stack = new Stack<>();
+  
+  DepthFirstSearch(int vertices){
+    adjList = new LinkedList[vertices];
+    visited = new boolean[vertices];
+    
+    for(int i = 0; i < vertices; i++){
+      adjList[i] = new LinkedList<Integer>(); 
+    }
+  }
+  void addEdge(int src, int dest){
+    adjList[src].add(dest);
+  }
+  void DFS(int vertex){
+    visited[vertex] = true;
+    System.out.print(vertex + " ");
+    
+    Iterator<Integer> it = adj[vertex].listIterator();
+    while(it.hasNext()){
+      int node = it.next();
+      if(!visited[node]){
+        DFS(node); 
+      }
+    }
+  }
+  boolean checkPath(int s, int d){
+    visited[s] = true;
+    stack.push(s);
+    
+    if(s == d){
+      return true; 
+    }
+    
+    Iterator<Integer> it = adj[s].listIterator();
+    while(it.hasNext()){
+      int node = it.next();
+      if(!visited[node]){
+        if(checkPath(node, d)){
+          return true; 
+        }
+      }
+    }
+    stack.pop();
+    return false; 
+  }
+}
 
+//Runtime o(V + E)
