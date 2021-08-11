d7889498e6efc83b54ac2cab58cf062cfe212afb
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-10 20:37:16 -0400

    Update LengthofLastWord.java

--- String/LengthofLastWord.java
+++ String/LengthofLastWord.java
@@ -1,1 +1,15 @@
-
+public class LengthofLastWord{
+  public static int lengthLastWord(String s){
+    int count = 0; 
+    boolean check = false; 
+    
+    for(int i = s.length - 1; i >= 0; i--){
+      if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+        check = true;
+        count++; 
+      }else if(check == true){
+        return count; 
+      }
+    }
+  }
+}
