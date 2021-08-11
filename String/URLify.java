224778d31228a142bf5cc86339c5d38b81d8b1ae
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-10 20:40:20 -0400

    Update URLify.java

--- String/URLify.java
+++ String/URLify.java
@@ -1,1 +1,31 @@
-
+public class URLify{
+  public static String url(char[] str, int length){
+    if(str == null){
+      return "";
+    }
+    
+    int space = 0; 
+    int index = 0; 
+    
+    for(int i = 0; i < length; i++){
+      if(str[i] == ' '){
+        space++;
+      }
+    }
+    
+    index = length + space * 2 - 1; 
+    
+    for(int i = length - 1; i >= 0; i--){
+      if(str[i] == ' '){
+        str[index] = '0';
+        str[index-1] = '2';
+        str[index-2] = '%';
+        index -= 3; 
+      }else{
+        str[index] = str[i];
+        index--; 
+      }
+    }
+    return new String(str).trim(); 
+  }
+}
