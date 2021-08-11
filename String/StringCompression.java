85cb23de1f9b294efa6c20fcf19e799ab76484c0
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-10 21:51:49 -0400

    Update StringCompression.java

--- String/StringCompression.java
+++ String/StringCompression.java
@@ -1,1 +1,25 @@
-
+public class StringCompression{
+  public static int compress(char[] chars){
+    if(chars == null || chars.length == 0){
+      return 0; 
+    }
+    
+    int index = 0, n = chars.length;
+    int left = 0, right = 0;
+    
+    while(left < n){
+      while(right < n && chars[left] == chars[right]){
+        right++;
+      }
+      int count = right - left;
+      chars[index++] = chars[left];
+      if(count > 1){
+        for(char c: String.valueOf(count).toCharArray()){
+          chars[index++] = c; 
+        }
+      }
+      left = right; 
+    }
+    return index; 
+  }
+}
