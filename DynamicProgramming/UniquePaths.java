01b17d3921cd8e5cc5be22cee2cb539f52023165
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-12 13:53:16 -0400

    Update UniquePaths.java

--- DynamicProgramming/UniquePaths.java
+++ DynamicProgramming/UniquePaths.java
@@ -1,1 +1,14 @@
-
+public class UniquePaths{
+  public static int uniquePaths(int m, int n, HashMap<String, Integer> memo){
+    String key = m + "," + n;
+    
+    if(!memo.containsKey(key)){
+      if(m == 1 || n == 1){
+        memo.put(key, 1);
+      }else{
+        memo.put(key, uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo));
+      }
+    }
+    return memo.get(key); 
+  }
+}
