1fdf9a4c27317f27117f683f00c644304cd2a4bb
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-12 12:12:31 -0400

    Update FibonacciSequence.java

--- DynamicProgramming/FibonacciSequence.java
+++ DynamicProgramming/FibonacciSequence.java
@@ -1,1 +1,17 @@
+public class FibonacciSequence{
+  public static int fib(int n){
+    int[] memo = new int[n+2];
+    
+    memo[0] = 0;
+    memo[1] = 1;
+    
+    for(int i = 2; i <= n; i++){
+      memo[i] = memo[i-1] + memo[i-2];
+    }
+    
+    return memo[n];
+  }
+}
 
+//Runtime o(n)
+//Space o(n)
