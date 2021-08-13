309bc95af34cca172ae29d5151668911bef5aa5f
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-12 22:19:31 -0400

    Update GroupAnagrams.java

--- String/GroupAnagrams.java
+++ String/GroupAnagrams.java
@@ -1,1 +1,16 @@
-
+public class GroupAnagrams{
+  public static List<List<String>> groupAnagrams(String[] strs){
+    Map<String, List<String>> map = new HashMap<>();
+    
+    for(String str : strs){
+      char[] arr = str.toCharArray();
+      Arrays.sort(arr);
+      String key = new Strin(arr);
+      if(!map.containsKey(key)){
+        map.put(key, new ArrayList<>());
+      }
+      map.get(key).add(str); 
+    }
+    return new ArrayList<>(map.values()); 
+  }
+}
