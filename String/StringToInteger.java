1e4928ae77d85d1c513c07ed1bbf93de26e68a04
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-14 21:19:42 -0400

    Update StringToInteger.java

--- String/StringToInteger.java
+++ String/StringToInteger.java
@@ -1,1 +1,26 @@
-
+public class StringToInteger{
+  public static int myAtoi(String s){
+    if(s == null || s.isEmpty()){
+      return 0; 
+    }
+    int sign = 1, index = 0, n = s.length();
+   
+    while(index < n && s.charAt(index) == ' '){
+      index++; 
+    } 
+    if(index >= n){
+      return 0; 
+    }
+    if(s.charAt(index) == '+' || s.charAt(index) == '-'){\
+      sign = s.charAt(index) == '+' ? 1 : -1; 
+    }
+    long res = 0; 
+    while(index < n && Character.isDigit(s.charAt(index))){
+      res = res * 10 + (s.charAt(index++) - '0');
+      if(res * sign > Integer.MAX_VALUE || res * sign < Integer.MIN_VALUE){
+        return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE; 
+      }
+    }
+    return (int) (res * sign); 
+  }
+}
