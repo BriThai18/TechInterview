65ae78698b1117cb8cab98614e4ddc9a3a1bfa86
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-14 22:40:40 -0400

    Update ClimbingStairs.java

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -1,1 +1,21 @@
-
+public class ClimbingStairs{
+  public static int climbStairs(int n){
+    Map<Integer, Integer> memo = new HashMap<>();
+    return helper(n, memo);
+  }
+  public static int helper(int n, Map<Integer, Integer> memo){
+    if(memo.containsKey(n)){
+      return memo.get(n); 
+    }
+    if(n == 0){
+      return 1; 
+    }
+    if(n < 0){
+      return 0; 
+    }
+    if(!memo.containsKey(n)){
+      memo.put(n, helper(n-1, memo) + helper(n-2, memo));
+    }
+    return memo.get(n); 
+  }
+}
