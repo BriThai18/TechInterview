3e89d4402a750f7fe53a718b7dfb3c564a57d01d
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-15 23:22:22 -0400

    Update MaximumSubarray.java

--- DynamicProgramming/MaximumSubarray.java
+++ DynamicProgramming/MaximumSubarray.java
@@ -1,1 +1,12 @@
-
+public class MaximumSubarray{
+  public static int maxSubarray(int[] nums){
+    int maxCur = nums[0];
+    int maxGlobal = nums[0];
+   
+    for(int i = 1; i < nums.length; i++){
+      maxCur = Math.max(nums[i], maxCur + nums[i]);
+      maxGlobal = Math.max(maxCur, maxGlobal);
+    }
+    return maxGlobal; 
+  }
+}
