58fc5df51ffb9b7ef39ec09dda087a33bdad45c6
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-15 23:24:56 -0400

    Update MaximumProductSubarray.java

--- DynamicProgramming/MaximumProductSubarray.java
+++ DynamicProgramming/MaximumProductSubarray.java
@@ -1,1 +1,16 @@
-
+public class MaximumProductSubarray{
+  public static int maxProduct(int[] nums){
+    if(nums == null || nums.length == 0){
+      return 0; 
+    }
+    
+    int maxVal = nums[0], minVal = nums[0], res = nums[0];
+    for(int i = 1; i < nums.length; i++){
+      int temp = maxVal * nums[i];
+      maxVal = Math.max(Math.max(nums[i] * maxVal, nums[i] * minVal), nums[i]);
+      minVal = Math.min(Math.min(temp, nums[i] * minVal), nums[i]);
+      res = Math.max(res, maxVal);
+    }
+    return res; 
+  }
+}
