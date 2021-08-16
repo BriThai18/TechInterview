9636e7fb261b64a41003d673832759662cd8e7b6
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-15 21:32:05 -0400

    Update PascalTriangle.java

--- DynamicProgramming/PascalTriangle.java
+++ DynamicProgramming/PascalTriangle.java
@@ -1,1 +1,24 @@
+public class PascalTriangle{
+  public static List<List<Integer>> generate(int numRows){
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    
+    if(numRows <= 0){
+      return res; 
+    }
+    
+    for(int i = 0; i < numRows; i++){
+      List<Integer> row = new ArrayList<>(); 
+      for(int j = 0; j < i+1; j++){
+        if(j == 0 || j == i){
+          row.add(1); 
+        }else{
+          row.add(res.get(i-1).get(j-1) + res.get(i-1).get(j));
+        }
+        res.add(row);
+      }
+    }
+    return res; 
+  }
+}
 
+//Runtime o(n^2)
