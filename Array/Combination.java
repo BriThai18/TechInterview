4df6203f2d5b8e22f7b74c5decb7226317897d5c
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-18 22:09:59 -0400

    Update Combination.java

--- Array/Combination.java
+++ Array/Combination.java
@@ -1,1 +1,20 @@
+public class Combination{
+  public static List<List<Integer>> combine(int n, int k){
+    List<List<Integer>> list = new ArrayList<>();
+    combination(list, new ArrayList<>(), 1, n, k);
+    return list; 
+  }
+  public static void combination(List<List<Integer>> list, List<Integer> temp, int start, int n, int k){
+    if(k == 0){
+      list.add(new ArrayList<>(temp));
+      return; 
+    }
+    for(int i = start; i <= n; i++){
+      temp.add(i);
+      combination(list, temp, i+1, n, k-1);
+      temp.remove(temp.size()-1); 
+    }
+  }
+}
 
+//Runtime o(n!/(n-k)!
