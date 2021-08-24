cc2f6e7f7c90863894c3c10e3a3e945672756539
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-23 20:47:36 -0400

    Update LetterCombinationsOfAPhoneNumber.java

--- String/LetterCombinationsOfAPhoneNumber.java
+++ String/LetterCombinationsOfAPhoneNumber.java
@@ -1,1 +1,37 @@
-
+public class LetterCombinationsOfAPhoneNumber{
+  public static List<String> letterCombinations(String digits){
+    String[] letters = {
+      "",
+      "",
+      "abc",
+      "def",
+      "ghi",
+      "jkl",
+      "mno",
+      "pqrs",
+      "tuv",
+      "wxyz",
+    };
+    List<String> result = new ArrayList<>(); 
+    
+    if(digits.length() == 0 || digits == null){
+      return result; 
+    }
+    result.add("");
+    
+    for(int i = 0; i < digits.length(); i++){
+      result = combine(letters[digits.charAt(i) - '0'], result);
+    }
+    return result; 
+  }
+  public static List<String> combine(String digit, List<String> l){
+    List<String> result = new ArrayList<>(); 
+    
+    for(int i = 0; i < digit.length(); i++){
+      for(String x : l){
+        result.add(x + digit.charAt(i));
+      }
+    }
+    return result; 
+  }
+}
