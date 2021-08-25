bbc6858249a548afa41a63f5363363c5722821a7
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-24 20:40:00 -0400

    Update LongestPalindrome.java

--- String/LongestPalindrome.java
+++ String/LongestPalindrome.java
@@ -1,1 +1,18 @@
-
+public class LongestPalindrome{
+  public static int longestPalindrome(String s){
+    int[] char_counts = new int[128];
+    int result = 0; 
+    
+    for(char c : s.toCharArray()){
+      char_counts[c]++;
+    }
+    
+    for(Integer num : char_counts){
+      result += num / 2 * 2; 
+      if(result % 2 == 0 && num % 2 == 1){
+        result += 1; 
+      }
+    }
+    return result; 
+  }
+}
