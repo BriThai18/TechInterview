17213f78698003c0969ca8c445f4b2e960dfad46
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-24 21:03:07 -0400

    Update FirstUniqueCharacter.java

--- Hashmap/FirstUniqueCharacter.java
+++ Hashmap/FirstUniqueCharacter.java
@@ -1,1 +1,20 @@
-
+public class FirstUniqueCharacterString{
+  public static int firstUniqueCharacter(String s){
+    Map<Character, Integer> map = new HashMap<>();
+    
+    for(char c : s.toCharArray()){
+      if(map.containsKey(c)){
+        map.put(c, map.get(c) + 1))
+      }else{
+        map.put(c, 1); 
+      }
+    }
+    
+    for(int i = 0; i < s.length(); i++){
+      if(map.get(s.charAt(i)) == 1){
+        return i; 
+      }
+    }
+    return -1; 
+  }
+}
