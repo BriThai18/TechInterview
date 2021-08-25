4faa9cff6d6e3532f28bd185bd5e2fc980f1106b
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-24 20:00:16 -0400

    Update LongestPalindromicSubstring.java

--- String/LongestPalindromicSubstring.java
+++ String/LongestPalindromicSubstring.java
@@ -1,1 +1,32 @@
-
+public class LongestPalindromicSubstring{
+  public static String longestPalindrome(String s){
+    if(s == null || s.length() < 1){
+      return "";
+    }
+    
+    int start = 0;
+    int end = 0; 
+    
+    for(int i = 0; i < s.length(); i++){
+      int len1 = helper(s, i, i);
+      int len2 = helper(s, i, i+1);
+      int len = Math.max(len1, len2);
+      if(len > end - start){
+        start = i - ((len - 1) / 2);
+        end = i + (len / 2); 
+      }
+    }
+    return s.substring(start, end + 1); 
+  }
+  public static int helper(String s, int start, int end){
+    if(s == null || start > end){
+      return 0; 
+    }
+    
+    while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+      start--; 
+      end++; 
+    }
+    return end - start - 1; 
+  }
+}
