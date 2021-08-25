27db756702a5134177161e0afeb3ae54cc1c05af
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-24 21:21:46 -0400

    Update MostCommonWord.java

--- Hashmap/MostCommonWord.java
+++ Hashmap/MostCommonWord.java
@@ -1,1 +1,24 @@
-
+public class MostCommonWord{
+  public static String mostCommonWords(String s, String[] banned){
+    Set<String> ban = new HashSet<>(Arrays.asList(banned));
+    Map<String, Integer> map = new HashMap<>();
+    int maxCount = 0; 
+    
+    String[] words = s.replaceAll("\\W+", " ").toLowerCase().split("\\s+");
+    
+    for(String word : words){
+      if(!ban.contains(word)){
+        int count = map.getOrDefault(w, 0) + 1;
+        maxCount = Math.max(maxCount, count);
+        map.put(w, count); 
+      }
+    }
+    
+    for(String key : map.KeySet()){
+      if(map.get(key) == maxCount){
+        return key; 
+      }
+    }
+    return null; 
+  }
+}
