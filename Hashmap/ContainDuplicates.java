17a8dc48a79d7881feb46d3c1ed43a91b8e0333c
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-25 21:19:39 -0400

    Update ContainDuplicates.java

--- Hashmap/ContainDuplicates.java
+++ Hashmap/ContainDuplicates.java
@@ -1,1 +1,16 @@
-
+public class ContainDuplicates{
+  public static boolean containsDuplicate(int[] nums){
+    Map<Integer, Integer> map = new HashMap<>();
+    
+    for(int num : nums){
+      map.put(num, 1 + map.getOrDefault(num, 0));
+    }
+    
+    for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+      if(entry.getValue() > 1){
+        return true; 
+      }
+    }
+    return false; 
+  }
+}
