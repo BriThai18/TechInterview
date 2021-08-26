809b8576c45b03da92ee6858734bb0cc455799c3
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-25 21:10:54 -0400

    Update SumOfUniqueElements.java

--- Hashmap/SumOfUniqueElements.java
+++ Hashmap/SumOfUniqueElements.java
@@ -1,1 +1,17 @@
-
+public class SumOfUniqueElements{
+  public static int sumOfUnique(int[] nums){
+    Map<Integer, Integer> map = new HashMap<>();
+    int sum = 0; 
+    
+    for(int num : nums){
+      map.put(num, 1 + map.getOrDefault(num, 0));
+    }
+    
+    for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+      if(entry.getValue() == 1){
+        sum += entry.getKey(); 
+      }
+    }
+    return sum; 
+  }
+}
