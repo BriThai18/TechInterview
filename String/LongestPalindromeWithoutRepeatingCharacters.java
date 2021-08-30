b9db31bc011ea700b589a330a50dd030a82aa0db
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-30 11:17:51 -0400

    Update LongestPalindromeWithoutRepeatingCharacters.java

--- String/LongestPalindromeWithoutRepeatingCharacters.java
+++ String/LongestPalindromeWithoutRepeatingCharacters.java
@@ -1,1 +1,20 @@
+public class LongestPalindromeWithoutRepeatingCharacters{
+  public static int lenghtOfLongestSubstring(String s){
+    int i = 0, j = 0;
+    int max = 0;
 
+    HashSet<Character> set = new HashSet<>();
+    
+    while(j < s.length()){
+      if(!set.contains(s.charAt(j)){
+        set.add(s.charAt(j));
+        j++;
+        max = Math.max(set.size(), max);
+      }else{
+        set.remove(s.charAt(i));
+        i++; 
+      }
+    }
+    return max;
+  }    
+}
