36440946c0245ef63ee44bf4f69ed63b066ca25b
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-08-29 23:32:36 -0400

    Create MaximumProductOfWordLengths.java

---
+++ String/MaximumProductOfWordLengths.java
@@ -0,0 +1,29 @@
+public class MaximumProductOfWordLengths{
+  public static int maxProduct(String[] words){
+    if(words == null || words.length == 0){
+      return 0; 
+    }
+    
+    int len = words.length;
+    int[] value = new int[len];
+    for(int i = 0; i < len; i++){
+      String tmp = words[i];
+      value[i] = 0;
+      for(int j = 0; j < words[i].length(); j++){
+        value[i] |= 1 << (tmp.charAt(j) - 'a');
+      }
+    }
+    
+    int max = 0;
+    for(int i = 0; i < len; i++){
+      for(int j = i+1; j < len; j++){
+        if((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > max)){
+          max = words[i].length() * words[j].length();
+        }
+      }
+    }
+    return max; 
+  }
+}
+
+//Runtime o(n^2)
