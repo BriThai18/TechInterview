30611e05a44abca0f2995a60aef626b7b88f3434
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-09-09 11:22:53 -0400

    Update SlowestKey.java

--- Array/SlowestKey.java
+++ Array/SlowestKey.java
@@ -1,1 +1,17 @@
+public class SlowestKey{
+  public static char slowestKey(int[] releaseTimes, String keysPressed){
+    int max = releaseTimes[0];
+    char c = keysPressed.charAt(0);
+    
+    for(int i = 1; i < releaseTimes.length; i++){
+      int temp = releaseTimes[i] - releaseTimes[i-1];
+      if(temp > max || (temp == max && keysPressed.charAt(i) > c)){
+        max = temp;
+        c = keysPressed.charAt(i);
+      }
+    }
+    return c; 
+  }
+}
 
+//Runtime o(n)
