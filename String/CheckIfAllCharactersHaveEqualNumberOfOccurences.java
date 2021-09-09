43d6584a67afca77d0e0b626604b227d26e9df2b
BriThai18 <42813644+BriThai18@users.noreply.github.com>
2021-09-09 12:14:23 -0400

    Update CheckIfAllCharactersHaveEqualNumberOfOccurences.java

--- String/CheckIfAllCharactersHaveEqualNumberOfOccurences.java
+++ String/CheckIfAllCharactersHaveEqualNumberOfOccurences.java
@@ -1,1 +1,18 @@
-
+public class CheckIfAllCharactersHaveEqualNumberOfOccurences{
+  public static boolean areOccurencesEqual(String s){
+    int[] freq = new int[26]; 
+    
+    for(char c : s.toCharArray()){
+      freq[c - 'a']++;
+    }
+    
+    int prev = freq[s.charAt(0) - 'a'];
+    
+    for(int num : freq){
+      if(num != 0 && num != prev){
+        return false; 
+      }
+    }
+    return true; 
+  }
+}
